c922a7fe69d38bb671a94c52f2bc0edf3eb58ea8
Aleksa Savić <unstoppablegamers87@gmail.com>
2022-03-06 15:19:08 +0100

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri("http://rxapi/WeatherForecast");
+                var response = await client.SendAsync(request);
+
+                ViewBag.JSON = await response.Content.ReadAsStringAsync();
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
