59b4428bfe655951f7c562ea307d085919791313
Aleksa Savić <unstoppablegamers87@gmail.com>
2022-03-26 11:37:53 +0100

    db-parser: reformat and cleanup code

--- src/db-parser/Program.cs
+++ src/db-parser/Program.cs
@@ -4,86 +4,73 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RxDBParser.Data;
 
-namespace RxDBParser
+namespace RxDBParser;
+
+internal class Program
 {
-    class Program
+    private static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        var context = new AstroContext();
+        await context.Database.ExecuteSqlRawAsync(
+            "delete from planet_positions7 where to_char(\"Time\", 'HH24:MI:SS') LIKE '00:00:00'");
+
+        string[] planets = { "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto" };
+
+        foreach (var planet in planets)
         {
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-            var context = new AstroContext();
-            await context.Database.ExecuteSqlRawAsync("delete from planet_positions7 where to_char(\"Time\", 'HH24:MI:SS') LIKE '00:00:00'");
-
-            string[] planets = { "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto" };
-
-            foreach (var planet in planets)
-            {
-                await FindRxPeriods(context, planet);
-                Console.WriteLine($"{planet} done...");
-            }
-                
+            await FindRxPeriods(context, planet);
+            Console.WriteLine($"{planet} done...");
         }
+    }
 
-        private static async Task FindRxPeriods(AstroContext context, string name)
+    private static async Task FindRxPeriods(AstroContext context, string name)
+    {
+        var startingTime = DateTime.Parse("2010-01-01 00:00:00"); // TODO: Hardcoded starting time
+
+        while (true)
         {
-            var startingTime = DateTime.Parse("2010-01-01 00:00:00");
+            var rxStart = await context.PlanetPositions.OrderBy(p => p.Time)
+                .FirstOrDefaultAsync(p => p.Retrograde == true && p.Time > startingTime && p.Name == name);
 
-            
-
-            while (true)
+            if (rxStart != null)
             {
-                var pocetakRx = await context.PlanetPositions.OrderBy(p => p.Time)
-                    .FirstOrDefaultAsync(p => p.Retrograde == true && p.Time > startingTime && p.Name == name);
+                var rxEnd = await context.PlanetPositions.OrderBy(p => p.Time)
+                    .FirstOrDefaultAsync(p => p.Retrograde == false && p.Time > rxStart.Time && p.Name == name);
 
-                //Console.WriteLine($"moguci pocetni: {pocetakRx.Time}");
-
-                if (pocetakRx != null)
+                if (rxEnd != null)
                 {
-                    var krajRx = await context.PlanetPositions.OrderBy(p => p.Time)
-                        .FirstOrDefaultAsync(p => p.Retrograde == false && p.Time > pocetakRx.Time && p.Name == name);
-
-                    if (krajRx != null)
+                    if ((rxEnd.Time - rxStart.Time).Days > 18) // && 
                     {
-                        // pocetakRx.Time != krajRx.Time - TimeSpan.FromMinutes(1)
-                        if ((krajRx.Time - pocetakRx.Time).Days > 18) // && 
+                        var periodExists = await context.RetrogradePeriods.AnyAsync(period =>
+                            period.Name == name && period.StartPosition.Time == rxStart.Time &&
+                            period.EndPosition.Time == rxEnd.Time);
+
+                        if (!periodExists)
                         {
-                            //Ispisi($"Poceo RX: {pocetakRx.Time}, {(krajRx.Time - pocetakRx.Time).Days}");
-                            //Ispisi($"Kraj RX: {krajRx.Time}");
-                            //Ispisi(
-                            //    $"{Environment.NewLine}--------------------------------------------{Environment.NewLine}");
-
-                            var periodExists = await context.RetrogradePeriods.AnyAsync(period =>
-                                period.Name == name && period.StartPosition.Time == pocetakRx.Time &&
-                                period.EndPosition.Time == krajRx.Time);
-
-                            if (!periodExists)
+                            var rxPeriod = new RetrogradePeriod
                             {
-                                var rxPeriod = new RetrogradePeriod()
-                                {
-                                    Name = name,
-                                    StartPosition = pocetakRx,
-                                    EndPosition = krajRx
-                                };
+                                Name = name,
+                                StartPosition = rxStart,
+                                EndPosition = rxEnd
+                            };
 
-                                context.Add(rxPeriod);
-                                await context.SaveChangesAsync();
-                            }
+                            context.Add(rxPeriod);
+                            await context.SaveChangesAsync();
                         }
+                    }
 
-                        startingTime = krajRx.Time;
-                    }
-                    else
-                    {
-                        //Ispisi("Nema vise krajnjeg RX-a!");
-                        break;
-                    }
+                    startingTime = rxEnd.Time;
                 }
                 else
                 {
-                    //Ispisi("Nema vise pocetnog RX-a!");
                     break;
                 }
+            }
+            else
+            {
+                break;
             }
         }
     }
